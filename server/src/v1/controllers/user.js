a998e6efffced635c118a319f14c17d15a2adf6b
daideguchi <dd.1107.11107@gmail.com>
2022-11-12 23:42:12 +0900

    ログイン機能まで実装

--- server/src/v1/controllers/user.js
+++ server/src/v1/controllers/user.js
@@ -1,4 +1,5 @@
 const CryptoJS = require("crypto-js");
+const { response } = require("express");
 const JWT = require("jsonwebtoken");
 const User = require("../models/user");
 
@@ -22,3 +23,41 @@ exports.register = async (req, res) => {
 };
 
 //ログインAPI
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    //DBからユーザーが存在するか探してくる
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      return res.status(401).json({
+        errors: {
+          param: "username",
+          message: "ユーザー名かパスワードが無効です",
+        },
+      });
+    }
+    //パスワードが合っているか照合
+    const descryptedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.SECRET_KEY
+    ).toString(CryptoJS.enc.Utf8);
+
+    if (descryptedPassword !== password) {
+      return res.status(401).json({
+        errors: {
+          param: "password",
+          message: "ユーザー名かパスワードが無効です",
+        },
+      });
+    }
+    //JWTを発行
+    const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+      expiresIn: "24h",
+    });
+
+    return res.status(201).json({ user, token });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
